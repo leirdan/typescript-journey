a69e26b36ca580fdaf0f1aa420b127486e0b2f7e
leirdan <uouleirdan@gmail.com>
2023-02-09 14:35:19 -0300

    Getters e Setters - exemplo com propriedades privadas

--- src/poo/gettersAndSetters.ts
+++ src/poo/gettersAndSetters.ts
@@ -19,16 +19,48 @@ class Book {
 	}
 
 	//setter
-	setTitle(setTitle: string) {
+	set setTitle(setTitle: string) {
 		this.title = setTitle;
 	}
-	setAuthor(setAuthor: string) {
+	set setAuthor(setAuthor: string) {
 		this.author = setAuthor;
 	}
 }
 
 const dune = new Book("Dune", "Tolkien");
-dune.setAuthor("Frank Herbert");
+dune.setAuthor = "Frank Herbert";
 
 console.log(dune.getTitle); // Dune
 console.log(dune.getAuthor); // Frank Herbert
+
+// Exemplo 2: com propriedades privadas
+
+class Student {
+	private name: string = "Andriel";
+	private id: number;
+	private course: string;
+
+	get getName(): string {
+		return this.name;
+	}
+
+	get getCourse(): string {
+		return this.course;
+	}
+
+	set setCourse(setCourse: string) {
+		this.course = setCourse;
+	}
+
+	constructor(id: number, course: string) {
+		this.id = id;
+		this.course = course;
+	}
+}
+
+const pupil = new Student(20204013901717, "Psychology");
+// única forma de acessarmos propriedades privadas de fora é utilizando getters
+console.log(pupil.getName); // Andriel
+console.log(pupil.getCourse); // Psychology
+pupil.setCourse = "IT";
+console.log(pupil.getCourse); // IT
